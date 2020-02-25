9a0d2941ef75099d3d300197de957d4431b6a29f
Paul Colley <drguru.track79@yahoo.co.uk>
2020-02-25 11:29:24 +0000

    inital relase

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+    
+    <div class="col-9 pt-2">
+    <div class="d-flex" >
+        <div class="pr-4"><strong>Bug ID</strong></div>
+        <div class="pr-4"><strong>Product </strong></div>
+        <div class="pr-4"><strong>OS </strong></div>
+        <div class="pr-4"><strong>Type</strong></div>
+        <div class="pr-4"><strong>Synopsis </strong></div>
+        <div class="pr-4"><strong>Assignment</strong></div>
+        <div class="pr-4"><strong>Date</strong></div>
+        <div class="pr-4"><strong>Time </strong></div>
+        <div class="pr-4"><strong>Status </strong></div>
+    </div>
+    </div>
+</div>
+@endsection
