a3cb7346dbe85eec980408a92d9fe751a54d673e
Paul Colley <drguru.track79@yahoo.co.uk>
2020-07-15 11:40:15 +0100

    added in table for display of bugs and projects - placeholder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,176 +10,87 @@
        @include('include/sidebar')
 
      <div class="main">
-        <div class="row">
-    <div class="col-12 pt-2 pl-5">
-    <div class="d-flex" >
-        <div class="pr-5"><strong>Bug ID</strong></div>
-        <div class="pr-5"><strong>Product </strong></div>
-        <div class="pr-5"><strong>OS </strong></div>
-        <div class="pr-5"><strong>Risk</strong></div>
-        <div class="pr-5"><strong>Synopsis </strong></div>
-        <div class="pr-5"><strong>Assignment</strong></div>
-        <div class="pr-5"><strong>Date</strong></div>
-        <div class="pr-5"><strong>Time </strong></div>
-        <div class="pr-5"><strong>Status </strong></div>
-    </div>
-    </div>
-    <div class="col-12 pt-2 pl-5">
-        <div class="d-flex" >
-            <div class="pr-4">#00001</div>
-            <div class="pr-4">Starbugged</div>
-            <div class="pr-5">W</div>
-            <div class="pr-5">Medium</div>
-            <div class="pr-5">Bug</div>
-            <div class="pr-5">A.N.Other</div>
-            <div class="pr-5">10/6/2020</div>
-            <div class="pr-5">09.47</div>
-            <div class="pr-5">In Progress</div>
-        </div>
-        </div>
-        <div class="col-12 pt-2 pl-5">
-            <div class="d-flex" >
-                <div class="pr-4">#00001</div>
-                <div class="pr-4">Starbugged</div>
-                <div class="pr-5">W</div>
-                <div class="pr-5">Medium</div>
-                <div class="pr-5">Bug</div>
-                <div class="pr-5">A.N.Other</div>
-                <div class="pr-5">10/6/2020</div>
-                <div class="pr-5">09.47</div>
-                <div class="pr-5">In Progress</div>
-            </div>
-            </div>
-            <div class="col-12 pt-2 pl-5">
-                <div class="d-flex" >
-                    <div class="pr-4">#00001</div>
-                    <div class="pr-4">Starbugged</div>
-                    <div class="pr-5">W</div>
-                    <div class="pr-5">Medium</div>
-                    <div class="pr-5">Bug</div>
-                    <div class="pr-5">A.N.Other</div>
-                    <div class="pr-5">10/6/2020</div>
-                    <div class="pr-5">09.47</div>
-                    <div class="pr-5">In Progress</div>
-                </div>
-                </div>
-                <div class="col-12 pt-2 pl-5">
-                    <div class="d-flex" >
-                        <div class="pr-4">#00001</div>
-                        <div class="pr-4">Starbugged</div>
-                        <div class="pr-5">W</div>
-                        <div class="pr-5">Medium</div>
-                        <div class="pr-5">Bug</div>
-                        <div class="pr-5">A.N.Other</div>
-                        <div class="pr-5">10/6/2020</div>
-                        <div class="pr-5">09.47</div>
-                        <div class="pr-5">In Progress</div>
-                    </div>
-                    </div>
-                    <div class="col-12 pt-2 pl-5">
-                        <div class="d-flex" >
-                            <div class="pr-4">#00001</div>
-                            <div class="pr-4">Starbugged</div>
-                            <div class="pr-5">W</div>
-                            <div class="pr-5">Medium</div>
-                            <div class="pr-5">Bug</div>
-                            <div class="pr-5">A.N.Other</div>
-                            <div class="pr-5">10/6/2020</div>
-                            <div class="pr-5">09.47</div>
-                            <div class="pr-5">In Progress</div>
-                        </div>
-                        </div>
-                        <div class="col-12 pt-2 pl-5">
-                            <div class="d-flex" >
-                                <div class="pr-4">#00001</div>
-                                <div class="pr-4">Starbugged</div>
-                                <div class="pr-5">W</div>
-                                <div class="pr-5">Medium</div>
-                                <div class="pr-5">Bug</div>
-                                <div class="pr-5">A.N.Other</div>
-                                <div class="pr-5">10/6/2020</div>
-                                <div class="pr-5">09.47</div>
-                                <div class="pr-5">In Progress</div>
-                            </div>
-                            </div>
-                            <div class="col-12 pt-2 pl-5">
-                                <div class="d-flex" >
-                                    <div class="pr-4">#00001</div>
-                                    <div class="pr-4">Starbugged</div>
-                                    <div class="pr-5">W</div>
-                                    <div class="pr-5">Medium</div>
-                                    <div class="pr-5">Bug</div>
-                                    <div class="pr-5">A.N.Other</div>
-                                    <div class="pr-5">10/6/2020</div>
-                                    <div class="pr-5">09.47</div>
-                                    <div class="pr-5">In Progress</div>
-                                </div>
-                                </div>
-                                <div class="col-12 pt-2 pl-5">
-                                    <div class="d-flex" >
-                                        <div class="pr-4">#00001</div>
-                                        <div class="pr-4">Starbugged</div>
-                                        <div class="pr-5">W</div>
-                                        <div class="pr-5">Medium</div>
-                                        <div class="pr-5">Bug</div>
-                                        <div class="pr-5">A.N.Other</div>
-                                        <div class="pr-5">10/6/2020</div>
-                                        <div class="pr-5">09.47</div>
-                                        <div class="pr-5">In Progress</div>
-                                    </div>
-                                    </div>
-                                    <div class="col-12 pt-2 pl-5">
-                                        <div class="d-flex" >
-                                            <div class="pr-4">#00001</div>
-                                            <div class="pr-4">Starbugged</div>
-                                            <div class="pr-5">W</div>
-                                            <div class="pr-5">Medium</div>
-                                            <div class="pr-5">Bug</div>
-                                            <div class="pr-5">A.N.Other</div>
-                                            <div class="pr-5">10/6/2020</div>
-                                            <div class="pr-5">09.47</div>
-                                            <div class="pr-5">In Progress</div>
-                                        </div>
-                                        </div>
-                                        <div class="col-12 pt-2 pl-5">
-                                            <div class="d-flex" >
-                                                <div class="pr-4">#00001</div>
-                                                <div class="pr-4">Starbugged</div>
-                                                <div class="pr-5">W</div>
-                                                <div class="pr-5">Medium</div>
-                                                <div class="pr-5">Bug</div>
-                                                <div class="pr-5">A.N.Other</div>
-                                                <div class="pr-5">10/6/2020</div>
-                                                <div class="pr-5">09.47</div>
-                                                <div class="pr-5">In Progress</div>
-                                            </div>
-                                            </div>
-                                            <div class="col-12 pt-2 pl-5">
-                                                <div class="d-flex" >
-                                                    <div class="pr-4">#00001</div>
-                                                    <div class="pr-4">Starbugged</div>
-                                                    <div class="pr-5">W</div>
-                                                    <div class="pr-5">Medium</div>
-                                                    <div class="pr-5">Bug</div>
-                                                    <div class="pr-5">A.N.Other</div>
-                                                    <div class="pr-5">10/6/2020</div>
-                                                    <div class="pr-5">09.47</div>
-                                                    <div class="pr-5">In Progress</div>
-                                                </div>
-                                                </div>
-                                                <div class="col-12 pt-2 pl-5">
-                                                    <div class="d-flex" >
-                                                        <div class="pr-4">#00001</div>
-                                                        <div class="pr-4">Starbugged</div>
-                                                        <div class="pr-5">W</div>
-                                                        <div class="pr-5">Medium</div>
-                                                        <div class="pr-5">Bug</div>
-                                                        <div class="pr-5">A.N.Other</div>
-                                                        <div class="pr-5">10/6/2020</div>
-                                                        <div class="pr-5">09.47</div>
-                                                        <div class="pr-5">In Progress</div>
-                                                    </div>
-                                                    </div>
+        <div class="row pl-3">
+            <table class="table table-bordered table-striped">
+                <tr>
+                    <th>Bug ID</th>
+                    <th>Product </th>
+                    <th>OS</th>
+                    <th>Risk</th>
+                    <th>Synopsis </th>
+                    <th>Assignment</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th>Status</th>
+                </tr>
+                <tr>
+                    <td><a href="">#00001</a></td>
+                    <td>Starbugged</td>
+                    <td>W</td>
+                    <td>Medium</td>
+                    <td>Bug</td>
+                    <td>A.N.Other</td>
+                    <td>10/6/2020</td>
+                    <td>09.47</td>
+                    <td>In Progress</td>
+                </tr>
+                <tr>
+                    <td><a href="">#00002</a></td>
+                    <td>Starbugged</td>
+                    <td>W</td>
+                    <td>Low</td>
+                    <td>Bug</td>
+                    <td>J.N.Other</td>
+                    <td>11/6/2020</td>
+                    <td>19.47</td>
+                    <td>In Progress</td>
+                </tr>
+                <tr>
+                    <td><a href="">#00003</a></td>
+                    <td>Warehouse</td>
+                    <td>W</td>
+                    <td>Low</td>
+                    <td>Bug</td>
+                    <td>K.N.Other</td>
+                    <td>1/7/2020</td>
+                    <td>20.47</td>
+                    <td>In Progress</td>
+                </tr>
+                <tr>
+                    <td><a href="">#00004</a></td>
+                    <td>Motoring</td>
+                    <td>W</td>
+                    <td>Low</td>
+                    <td>Bug</td>
+                    <td>S.N.Other</td>
+                    <td>11/6/2020</td>
+                    <td>19.47</td>
+                    <td>In Progress</td>
+                </tr>
+                <tr>
+                    <td><a href="">#00005</a></td>
+                    <td>Starbugged</td>
+                    <td>W</td>
+                    <td>Low</td>
+                    <td>Issue</td>
+                    <td>J.N.Other</td>
+                    <td>11/6/2020</td>
+                    <td>6.25</td>
+                    <td>In Progress</td>
+                </tr>
+                <tr>
+                    <td><a href="">#00006</a></td>
+                    <td>Starbugged</td>
+                    <td>W</td>
+                    <td>Low</td>
+                    <td>Bug</td>
+                    <td>J.N.Other</td>
+                    <td>11/6/2020</td>
+                    <td>8.59</td>
+                    <td>Resolved</td>
+                </tr>
+            </table>
+
 </div>
 <div>
 @endsection
