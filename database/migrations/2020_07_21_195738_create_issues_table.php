0b0fa6acd0c925e5d97c150aa1c82a0c7ec47a8b
Paul Colley <drguru.track79@yahoo.co.uk>
2020-07-21 21:32:52 +0100

    created projects and issues models

---
+++ database/migrations/2020_07_21_195738_create_issues_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIssuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('issues', function (Blueprint $table) {
+            $table->bigIncrements('bug_id');
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->string('os');
+            $table->string('risk');
+            $table->string('issue');
+            $table->string('description');
+            $table->string('assignment');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('issues');
+    }
+}
