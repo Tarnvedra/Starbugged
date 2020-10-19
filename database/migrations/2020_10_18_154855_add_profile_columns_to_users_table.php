ae09bc8916ba2e6b4daa8b8f1da1aa86a89c54cd
Paul Colley <drguru.track79@yahoo.co.uk>
2020-10-19 11:30:29 +0100

    changed migration to allow nullable update users table

--- database/migrations/2020_10_18_154855_add_profile_columns_to_users_table.php
+++ database/migrations/2020_10_18_154855_add_profile_columns_to_users_table.php
@@ -14,9 +14,9 @@ class AddProfileColumnsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('job_title')->after('username');
-            $table->string('profile_image')->after('job_title');
-            $table->text('about_me')->after('profile_image');
+            $table->string('job_title')->after('username')->nullable();;
+            $table->string('profile_image')->after('job_title')->nullable();;
+            $table->text('about_me')->after('profile_image')->nullable();;
         });
     }
 
