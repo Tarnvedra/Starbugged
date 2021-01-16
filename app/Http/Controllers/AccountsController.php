dc8ab4b15775d08d4db48aeb9e60b05934d0dde1
Paul Colley <drguru.track79@yahoo.co.uk>
2021-01-16 14:46:07 +0000

    use of factories in accounts controller and general tidy up  part 1

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use Illuminate\Routing\ResponseFactory;
+use Illuminate\Contracts\View\Factory as ViewFactory;
+use Illuminate\Contracts\View\View;
 use App\User;
 use App\Role;
 use App\Project;
@@ -11,187 +15,120 @@ use App\Issue;
 class AccountsController extends Controller
 {
 
-    public function __construct() {
+    public function __construct()
+    {
 
         $this->middleware('auth');
     }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+
+    public function index(ViewFactory $view)
     {
-           // $user = Auth::findOrFail(user());
-           $user = auth()->user();
-           return view('admin/account')->with('user' , $user);
+        $user = auth()->user();
+        return $view->make('admin/account', ['user' => $user]);
     }
 
-    public function admin()
+    public function admin(ViewFactory $view)
     {
-            //$user = Auth::findOrFail(user());
-           // dd($user);
-            $user = auth()->user();
-            $users = User::orderBy('username','asc')->paginate(11);
-            $user_roles = Role::get();
-            return view('admin/manageusers')->with('users', $users)->with('user', $user)->with('user_roles' , $user_roles);
+        $user = auth()->user();
+        $users = User::orderBy('username', 'asc')->paginate(11);
+        $user_roles = Role::get();
+        return $view->make('admin/manageusers',
+            [   'users' => $users,
+                'user' => $user,
+                'user_roles' => $user_roles
+            ]);
     }
 
-
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function edit(ViewFactory $view,$id)
     {
-        //
+        $user = auth()->user();
+        $editeduser = User::find($id);
+        $roles = Role::all();
+        return $view->make('admin/edit' ,
+        [
+            'editeduser' => $editeduser,
+            'roles' => $roles,
+            'user' => $user
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function update( Request $request,ResponseFactory $repspone, $id)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-
-    $user = auth()->user();
-    $editeduser = User::find($id);
-    $roles = Role::all();
-    return view('admin/edit')->with([
-        'editeduser' => $editeduser,
-        'roles' => $roles
-    ])->with('user' , $user);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $this->validate($request,[
+        $this->validate($request, [
             'username' => 'required',
 
             'useraccountlevel' => 'required',
 
         ]);
-    $user_update = User::find($id);
-    //$roles = Role::find($user->id);
-    $user_update->username = $request->input('username');
-    //$roles->roles = $request->input('roles');
-    $user_update->useraccountlevel = $request->input('useraccountlevel');
-    $user_update->save();
-    return redirect()->action('AccountsController@admin');
-
+        $user_update = User::find($id);
+        //$roles = Role::find($user->id);
+        $user_update->username = $request->input('username');
+        //$roles->roles = $request->input('roles');
+        $user_update->useraccountlevel = $request->input('useraccountlevel');
+        $user_update->save();
+        return $repspone->redirectTo('AccountsController@admin');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-
-    public function dashboard()
+    public function dashboard(ViewFactory $view)
     {
         $user = auth()->user();
-        $projects = Project::orderBy('id','asc')->paginate(10);
+        $projects = Project::orderBy('id', 'asc')->paginate(10);
         $issues = Issue::count();
         $projectsCount = Project::count();
         $status = Issue::where('status', '!=', 'resolved')->count();
-        $priority = Issue::where('risk' ,'=','High')->count();
+        $priority = Issue::where('risk', '=', 'High')->count();
 
         // get percentage values / check to avoid for divison by 0
-        if ($status >0 && $issues >0) {
-        $statusPercentage = ($status /$issues) * 100;
-        $statusPercentage = round($statusPercentage, 0);}
-        else {
+        if ($status > 0 && $issues > 0) {
+            $statusPercentage = ($status / $issues) * 100;
+            $statusPercentage = round($statusPercentage, 0);
+        } else {
             $statusPercentage = 0;
         }
 
-        if ($priority >0 && $issues >0) {
-        $priorityPercentage = ($priority / $issues) * 100;
-        $priorityPercentage = round($priorityPercentage, 0); }
-        //dd($issues, $status, $amount);
+        if ($priority > 0 && $issues > 0) {
+            $priorityPercentage = ($priority / $issues) * 100;
+            $priorityPercentage = round($priorityPercentage, 0);
+        }
         else {
             $priorityPercentage = 0;
         }
 
-        return view('home' , [ 'user' => $user,
-         'projects' => $projects,
-         'issues' => $issues,
-         'projectscount' => $projectsCount,
-         'priority' => $priority,
-         'status' => $status,
-         'statuspercentage' => $statusPercentage,
-         'prioritypercentage' => $priorityPercentage ]);
+        return $view->make('home',
+            [ 'user' => $user,
+              'projects' => $projects,
+              'issues' => $issues,
+              'projectscount' => $projectsCount,
+              'priority' => $priority,
+              'status' => $status,
+              'statuspercentage' => $statusPercentage,
+              'prioritypercentage' => $priorityPercentage]);
     }
 
-    public function profile()
+    public function profile(ViewFactory $view)
     {
-
-           $user = auth()->user();
-
-           return view('admin/profile')->with('user' , $user);
-
+        $user = auth()->user();
+        return $view->make('admin/profile' , ['user' => $user]);
     }
 
-    public function storeProfile(Request $request)
+    public function storeProfile(Request $request, ResponseFactory $response)
     {
-        $this->validate($request,[
+        $this->validate($request, [
             'jobtitle' => 'required',
-            ]);
-
-    $user = auth()->user();
-    $user->job_title = $request->input('jobtitle');
-    // will need image file for this
-    $user->profile_image = $request->input('profileimage');
-    $user->about_me = $request->input('aboutme');
-    $user->update();
-    return redirect()->action('AccountsController@profile');
+        ]);
+        $user = auth()->user();
+        $user->job_title = $request->input('jobtitle');
+        // will need image file for this
+        $user->profile_image = $request->input('profileimage');
+        $user->about_me = $request->input('aboutme');
+        $user->update();
+        return $response->redirectTo('AccountsController@profile');
 
     }
 
-    public function updateProfile()
+    public function updateProfile(ViewFactory $view)
     {
-
-    $user = auth()->user();
-    return view('admin/editprofile')->with('user' , $user);
-
+        $user = auth()->user();
+        return $view->make('admin/editprofile' , ['user' => $user]);
     }
-
-
 }
