77a4a3b6b8a1ebd8c8ea12a02cfc2b4cef5b2531
Paul Colley <drguru.track79@yahoo.co.uk>
2021-01-16 16:06:42 +0000

    added admin datatable for bootstrap vue

---
+++ resources/js/components/AdminDatatable.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('Status mounted.');
+    },
+    data() {},
+    created(){
+        this.fetchData();
+    },
+
+    methods: {}
+}
+</script>
