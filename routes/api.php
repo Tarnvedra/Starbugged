0838c45c036821deee52cbfdf66abeddd3f77b9b
Paul Colley <drguru.track79@yahoo.co.uk>
2021-02-02 19:44:16 +0000

    refactor of api's

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
+
 use Illuminate\Http\Request;
+use App\Http\Controllers\Priorities\PrioritiesController;
+use App\Http\Controllers\Status\StatusController;
+use App\Http\Controllers\Watching\WatchingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,17 +20,20 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-// prirorties api
-Route::get('/priorities' , 'PrioritiesController@index'); //get all issues by priority
-Route::get('/priorities/low' , 'PrioritiesController@getLowPriority'); //get all issues with low priority status
-Route::get('/priorities/medium' , 'PrioritiesController@getMedPriority'); //get all issues with medium priority status
-Route::get('/priorities/high' , 'PrioritiesController@getHighPriority'); //get all issues with high priority status
-Route::get('/priorities/{id}' , 'PrioritiesController@show'); //get one issue
+
+// priorities api
+Route::get('/priorities', [PrioritiesController::class, 'index']); //get all issues by priority
+Route::get('/priorities/low', [PrioritiesController::class, 'getLowPriority']); //get all issues with low priority status
+Route::get('/priorities/medium', [PrioritiesController::class, 'getMediumPriority']); //get all issues with medium priority status
+Route::get('/priorities/high', [PrioritiesController::class, 'getHighPriority']); //get all issues with high priority status
+Route::get('/priorities/{id}', [PrioritiesController::class, 'show']); //get one issue
+
 // status api
-Route::get('/status' , 'StatusController@index'); //get all issues by status
-Route::get('/status/created' , 'StatusController@created'); // get all issues by issue created status
-Route::get('/status/progress' , 'StatusController@progress'); // get all issues by in progress status
-Route::get('/status/resolved' , 'StatusController@resolved'); // get all issues by in resolved status
-Route::get('/status/{id}' , 'StatusController@show'); //get one issue
+Route::get('/status', [StatusController::class, 'index']); //get all issues by status
+Route::get('/status/created', [StatusController::class, 'created']); // get all issues by issue created status
+Route::get('/status/progress', [StatusController::class, 'progress']); // get all issues by in progress status
+Route::get('/status/resolved', [StatusController::class, 'resolved']); // get all issues by in resolved status
+Route::get('/status/{id}', [StatusController::class, 'show']); //get one issue
+
 // watch api
-//Route::post('/watch/{id}' , 'WatchingController@store');
+//Route::post('/watch/{id}', [WatchingController::class, 'store']);
