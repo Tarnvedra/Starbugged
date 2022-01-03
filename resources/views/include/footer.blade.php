2d52ffeb0738770ce1a8489cd7e9a0a32d7bbd13
Paul Colley <drguru.track79@yahoo.co.uk>
2022-01-03 15:45:45 +0000

    added software version to footer

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
       <div class="copyright pull-left my-auto">
-        <span>&copy; 2021 Starbugged v0.71 - d4747d</span>
+          <span>&copy; 2022 Starbugged {{config('version.string')}} </span>
       </div>
     </div>
   </footer>
